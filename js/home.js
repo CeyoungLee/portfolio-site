fbbee040444d63bf18b9c23aebfde6e4b93e7062
CeyoungLee <cey.o.lee@gmail.com>
2020-01-02 03:11:19 +0900

    homeModule modulified. bg shows.

---
+++ js/home.js
@@ -0,0 +1,20 @@
+export const home = document.querySelector("#home");
+
+const IMG_NUMBER = 1;
+
+function paintImg(imgNumber) {
+  home.style.background=`aquamarine url('./src/${imgNumber}.jpg') no-repeat left top`;
+  home.style.backgroundSize=`100% 100%`;
+}
+
+function genRandom() {
+  return Math.floor((Math.random() * IMG_NUMBER) + 1);
+}
+
+export function init() {
+  const randomNumber = genRandom();
+  paintImg(randomNumber);
+  console.log("init");
+}
+
+
--- js/main.js
+++ js/main.js
@@ -1,27 +1,9 @@
 const skip = document.querySelector("#skip");
 const nav = document.querySelector("#nav");
-const home = document.querySelector("#home");
 const about = document.querySelector("#about");
 const works = document.querySelector("#works");
 const contacts = document.querySelector("#contacts");
 const footer = document.querySelector("#footer");
 
-const IMG_NUMBER = 1;
-
-function paintImg(imgNumber) {
-	home.style.background=`aquamarine url('./src/${imgNumber}.jpg') no-repeat left top`;
-	home.style.backgroundSize=`100% 100%`;
-}
-
-function genRandom() {
-	const number = Math.floor((Math.random() * IMG_NUMBER) + 1);
-	return number;
-}
-
-function init() {
-	const randomNumber = genRandom();
-	paintImg(randomNumber);
-
-}
-
-init();
+import * as homeMod from "./home.js";
+homeMod.init();
