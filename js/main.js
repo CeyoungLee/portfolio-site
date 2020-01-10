e7adb93025b06732307e12047f84a1c95bb6e4cb
CeyoungLee <cey.o.lee@gmail.com>
2020-01-10 19:13:33 +0900

    integrated in one (for cleaning codes

--- js/main.js
+++ js/main.js
@@ -4,8 +4,55 @@ const works = document.querySelector("#works");
 const contacts = document.querySelector("#contacts");
 const footer = document.querySelector("#footer");
 
-import * as homeMod from "./home.js";
-homeMod.init();
+//home
+export const home = document.querySelector("#home");
 
-import * as navMod from "./nav.js";
-navMod.init();
+const IMG_NUMBER = 1;
+
+
+
+
+function paintImg(imgNumber) {
+  home.style.background=`aquamarine url('./src/${imgNumber}.jpg') no-repeat left top`;
+  home.style.backgroundSize=`100% 100%`;
+}
+
+function genRandom() {
+  return Math.floor((Math.random() * IMG_NUMBER) + 1);
+}
+
+export function homeInit() {
+  const randomNumber = genRandom();
+  paintImg(randomNumber);
+  console.log("home.init");
+}
+homeInit();
+
+
+//nav
+
+export const nav = document.querySelector("#nav");
+const navUl=document.childNodes;
+
+const NAV_BTN_CLASS = "fa fa-bars";
+
+function paintNavBtn() {
+  const navBtn = document.createElement('i');
+  navBtn.innerText=" ";
+  navBtn.classList.add(NAV_BTN_CLASS);
+
+}
+
+function showList() {
+  const navHome = document.createElement("li");
+  navHome.innerHTML="HOME";
+  navUl.appendChild(navHome);
+}
+
+
+export function navInit() {
+  showList();
+  paintNavBtn();
+  console.log("nav.init");
+}
+navInit();
